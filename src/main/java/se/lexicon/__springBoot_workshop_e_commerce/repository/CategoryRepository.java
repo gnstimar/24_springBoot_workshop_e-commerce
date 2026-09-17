3c8ec9cf0e81e14059c32a3b6d42d3c3b00e6ba4
Agnes Timar <timaragnes@outlook.hu>
2026-09-17 15:27:30 +0200

    Repository layer: CategoryRepository

---
+++ src/main/java/se/lexicon/__springBoot_workshop_e_commerce/repository/CategoryRepository.java
@@ -0,0 +1,18 @@
+package se.lexicon.__springBoot_workshop_e_commerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.lexicon.__springBoot_workshop_e_commerce.entitiy.Category;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+    Optional<Category> findByNameIgnoreCase(String categoryName);
+
+    boolean existsByName(String categoryName);
+
+    // === Optional ===
+    List<Category> findByNameContaining(String word);
+
+    long count();
+}
