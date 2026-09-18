2784d2c45b52ab9a6201c5f391fe8ff86de0872d
Agnes Timar <timaragnes@outlook.hu>
2026-09-18 15:24:40 +0200

    Repository layer: OrderRepository (solving N+1 problem)

--- src/main/java/se/lexicon/__springBoot_workshop_e_commerce/repository/OrderRepository.java
+++ src/main/java/se/lexicon/__springBoot_workshop_e_commerce/repository/OrderRepository.java
@@ -1,6 +1,9 @@
 package se.lexicon.__springBoot_workshop_e_commerce.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import se.lexicon.__springBoot_workshop_e_commerce.entitiy.Order;
 import se.lexicon.__springBoot_workshop_e_commerce.entitiy.OrderStatus;
@@ -14,6 +17,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByCustomer_Id(Long customerId);
 
     // TODO Find orders by status and use a strategy to avoid the N+1 problem (loading order items in the same query).
+    // EntityGraph
+    /*@EntityGraph(attributePaths = {"items"})
+    List<Order> findByOrderStatus(OrderStatus orderStatus);*/
+    @Query("SELECT DISTINCT o FROM Order o JOIN FETCH o.items WHERE o.orderStatus = :status")
+    List<Order> findByOrderStatusWithItems(@Param("status") OrderStatus orderStatus);
 
     // === Optional ===
     List<Order> findByOrderDateAfter(Instant date);
